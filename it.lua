3d2821ea536a5c8412db98f345cbaf0e8e55ee41
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-04 13:40:41 -0500

    Adds extra spacing between it and requires

--- it.lua
+++ it.lua
@@ -1,6 +1,7 @@
 local expect = require 'expect'
 local colors = require 'ansicolors'
 local symbols = require 'symbols'
+
 local function it(msg, test_fn)
     local status, err = pcall(test_fn, expect)
     print(symbols:icon('entry')..msg, status and colors'%{green}Passed' or '%{red}Failed', err or '')
