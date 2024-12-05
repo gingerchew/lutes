4467135ab7411a9b4a95644feb02b2e5a42cc164
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-05 10:50:54 -0500

    Adds dur and implements coroutines to Describe
    I'm not huge on coroutines and when they're useful
    For now this is just a "did this work? Okay cool"
    
    I imagine it will be more useful inside of :run
    but for now this is fine

--- describe.lua
+++ describe.lua
@@ -11,35 +11,51 @@ function Describe:new(msg, desc_fn)
     self.__fns = {}
 
     desc_fn(function(test_msg, test_fn)
-        self:store_fn(test_msg, test_fn)
+        self.__fns[test_msg] = test_fn
     end, expect)
-    
-    local print_msg = self:run(msg)
+    -- messing with coroutines
+    -- is this useful? is this good? to be seen
+    -- I could see it being more useful inside of :run
+    local co = coroutine.create(function()
+        local print_msg = self:run(msg)
+        coroutine.yield(print_msg)
+    end)
+
+    local _, print_msg = coroutine.resume(co)
 
     print(print_msg)
 end
 
-function Describe:store_fn(msg, it_fn)
-    self.__fns[msg] = it_fn
+-- Simple duration passed function
+---@TODO: Format the string better
+function Describe:dur()
+    local start_time = os.clock()
+
+    return function()
+        return colors("%{dim}"..string.format("%.2f", (os.clock() - start_time) * 1000)..'ms ')
+    end
 end
 
 function Describe:run(msg)
     local has_errors = false
     local msgs = ''
 
+    -- maybe this could be moved into a coroutine?
     for test_msg, test_fn in pairs(self.__fns) do
         local is_final = next(self.__fns, test_msg) == nil
-
+        local get_dur = self:dur()
         local status, err = pcall(test_fn, expect)
+
 
         if not status then
             has_errors = true
-            msgs = msgs..'\n'..symbols:icon(is_final and 'entry_error_final' or 'entry_error')..test_msg..colors("%{red}%{bright} "..err)
+            msgs = msgs..'\n'..symbols:icon(is_final and 'entry_error_final' or 'entry_error')..get_dur()..test_msg..colors("%{red}%{bright} "..err)
         else
-            msgs = msgs..'\n'..symbols:icon(is_final and 'entry_final' or 'entry')..test_msg..colors('%{green}%{bright} Passed')
+            msgs = msgs..'\n'..symbols:icon(is_final and 'entry_final' or 'entry')..get_dur()..test_msg..colors('%{green}%{bright} Passed')
         end
     end
 
+    -- string formatting like this sucks
     local print_msg = '\n'
     if has_errors then
         print_msg = print_msg..symbols:icon('error_start')..colors('%{bright}%{red}'..msg)..'\n'
@@ -49,46 +65,6 @@ function Describe:run(msg)
 
     return print_msg..symbols:icon('wall')..msgs
 end
-
--- Can this be done without making a huge concated string??
--- this function is doing a lot lmao
--- fix that
---[[local function describe(msg, desc_fn)
-    -- cue all the tests here first
-    local test_table = {}
-    local function it(test_msg, test_fn)
-        test_table[test_msg] = test_fn
-    end
-    -- prepare msgs var
-    local msgs = ''
-    local has_errors = false
-
-    -- run tests passing in the cue-ing it function and expect
-    desc_fn(it, expect)
-    for test_msg, test_fn in pairs(test_table) do
-        local is_final = next(test_table, test_msg) == nil
-        local start_time = os.clock()
-        -- to match functionality with `test.it` pass expect at this level again
-        local status, err = pcall(test_fn, expect)
-        -- need to format this better and make it not the responsibility of describe
-        local dur = colors("%{dim}"..string.format("%.2f", (os.clock() - start_time) * 1000)..'ms ')
-        if not status then
-            has_errors = true
-            msgs = msgs..'\n'..symbols:icon(is_final and 'entry_error_final' or 'entry_error')..dur..test_msg..' '..colors('%{red}%{bright}'..err)
-        else
-            msgs = msgs..'\n'..symbols:icon(is_final and 'entry_final' or 'entry')..dur..test_msg..' '..colors('%{green}%{bright}Passed')
-        end
-    end
-    local print_msg = '\n'
-    if has_errors then
-        print_msg = print_msg..symbols:icon('error_start')..colors('%{bright}%{red}'..msg)..'\n'
-    else
-        print_msg = print_msg..symbols:icon('start')..colors('%{bright}%{green}'..msg)..'\n'
-    end
-    print_msg = print_msg..symbols:icon('wall')..msgs
-    print(print_msg)
-    print(symbols.space)
-end]]
 
 local function describe(msg, desc_fn)
     Describe:new(msg, desc_fn)
